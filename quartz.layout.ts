5f40d9fb26763702ebf1d811a242886c64276f56
Jake <jakemorgan108@outlook.com>
2025-05-06 13:43:28 -0400

    Quartz sync: May 6, 2025, 1:43 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/GameCodeMastery/documentation",
+      GitHub: "https://github.com/GameCodeMastery",
       "Discord Community": "https://discord.gg/DQWbDHJ",
     },
   }),
